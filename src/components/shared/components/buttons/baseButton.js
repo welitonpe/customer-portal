c63c56012fa29b4309eb904d0c22d016ad5183ce
Jair Medeiros <jair.medeiros@liferay.com>
2021-09-09 17:19:04 -0300

    Add icon to BaseButton

--- src/components/shared/components/buttons/baseButton.js
+++ src/components/shared/components/buttons/baseButton.js
@@ -1,11 +1,13 @@
 import { Button } from "@clayui/core";
+import ClayIcon from '@clayui/icon';
 
-const BaseButton = ({ onClick, text }) => {
+const BaseButton = ({ onClick, text, styles, preffixIcon }) => {
     return (
-        <Button displayType={null} onClick={onClick}>
+        <Button displayType={null} onClick={onClick} className={styles}>
+            { preffixIcon && <ClayIcon className="mr-1" symbol={preffixIcon} />}
             { text }
         </Button>
     );
 }
 
-export default BaseButton
+export default BaseButton;
