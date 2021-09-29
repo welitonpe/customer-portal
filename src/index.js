4a22074441241703300cfb15c98a7d34b5947fba
Jair Medeiros <jair.medeiros@liferay.com>
2021-09-29 17:45:12 -0300

    Fix getApps function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import { getApps } from '~/apps';
 import kebabize from '~/shared/utils/kebabize'
 
 const TAG_PROJECT = "cp";
-const apps = getApps();
 
 const getTagName = (componentName) => {
   return TAG_PROJECT + "-" + kebabize(componentName);
 };
 
-apps.forEach(app => {
+getApps().forEach(app => {
   const tagName = getTagName(app.name);
 
   if (!customElements.get(tagName)) {
