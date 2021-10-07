6546ab893012e88a31cc185f046e7d41a77ed419
Aguinus Santana <aguinus.santana@liferay.com>
2021-10-07 14:07:24 -0300

    Add new card layout for more than 5 projects

--- src/apps/customer-portal/pages/home.js
+++ src/apps/customer-portal/pages/home.js
@@ -1,29 +1,13 @@
 import ProjectCard from "../components/ProjectCard";
+import ProjectCardFind from "../components/ProjectCardFind";
 
 const projects = [
   {
     title: "Super1",
-    status: "Not",
+    status: "Active",
     endDate: "Jul 19, 2050",
-    state: "Brazil"
-  },
-  {
-    title: "Super2",
-    status: "Yes",
-    endDate: "Jul 30, 2050",
-    state: "Curado 1"
-  },
-  {
-    title: "Super3",
-    status: "Yes",
-    endDate: "Jul 30, 2050",
-    state: "Curado 2"
-  },
-  {
-    title: "Super4",
-    status: "Yes",
-    endDate: "Jul 30, 2050",
-    state: "Curado 4"
+    state: "United States",
+    subtitle: "Digitals"
   }
 ];
 
@@ -33,6 +17,8 @@ const Home = () => {
       <h1>Projects</h1>
       <div className="d-flex flex-row flex-wrap">
         {projects.map((project, index) => <ProjectCard key={index} title={project.title} status={project.status} endDate={project.endDate} state={project.state} />)}
+
+        {projects.map((project, index) => <ProjectCardFind key={index} title={project.title} status={project.status} endDate={project.endDate} state={project.state} subtitle={project.subtitle} />)}
       </div>
     </>
   );
