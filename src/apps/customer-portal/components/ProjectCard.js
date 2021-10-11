14e0a62f0a73742fbc606514995ce5acb55d0fbd
Jair Medeiros <jair.medeiros@liferay.com>
2021-10-10 23:04:33 -0300

    Fix styles to attend the dialect style theme

--- src/apps/customer-portal/components/ProjectCard.js
+++ src/apps/customer-portal/components/ProjectCard.js
@@ -1,28 +1,28 @@
-import { Button } from "@clayui/core";
 import ClayCard from '@clayui/card';
-import ClayLabel from '@clayui/label';
-import ClayIcon from '@clayui/icon';
+import BaseButton from '~/shared/components/BaseButton';
+import StatusTag from './StatusTag';
 
 const ProjectCard = ({ title, status, endDate, state, onClick, small }) => {
 
   return (
-    <ClayCard className={`project-card m-0${small ? " sm" : ""}`} onClick={() => onClick}>
-      <ClayCard.Body className={`d-flex flex-${small ? "row" : "column"} h-100 justify-content-between${small ? " sm" : ""}`}>
-        <ClayCard.Description displayType="title" tag={`${small ? "h4" : "h3"}`} title={null}>
+    <ClayCard className={`project-card${small ? "-sm" : ""} m-0`} onClick={() => onClick}>
+      <ClayCard.Body className={`d-flex flex-${small ? "row" : "column"} h-100 justify-content-between`}>
+        <ClayCard.Description className="text-neutral-3" displayType="title" tag={`${small ? "h4" : "h3"}`}>
           {title}
         </ClayCard.Description>
-        <div className="d-flex justify-content-between align-items-end">
-          <ClayCard.Description className={`info-project${small ? " sm" : ""}`} truncate={false} displayType="text" tag="div" title={null}>
-            <ClayLabel className="border-0 text-capitalize font-weight-normal m-0" displayType="success">{status}</ClayLabel>
-            <div className={`info-date${small ? " sm" : ""}`}>
-              Ends on <span className="font-weight-bold end-date">{endDate}</span>
+        <div className={`${small ? "" : "align-items-end "}d-flex justify-content-between`}>
+          <ClayCard.Description truncate={false} displayType="text" tag="div" title={null}>
+            <StatusTag currentStatus={status} />
+            <div className={`text-paragraph-sm text-neutral-5${small ? " sm-mb" : " my-1"}`}>
+              Ends on <span className="text-paragraph font-weight-bold">{endDate}</span>
             </div>
-            {state}
+            <div className="text-paragraph-sm text-neutral-5">
+              {state}
+            </div>
           </ClayCard.Description>
-          {!small && <Button className="p-0 text-decoration-none" displayType="link">
+          {!small && <BaseButton className="p-0 text-brand-primary" appendIcon="angle-right" borderless>
             See details
-            <ClayIcon className="ml-1" symbol="angle-right" />
-          </Button>}
+          </BaseButton>}
         </div>
       </ClayCard.Body>
     </ClayCard>
